8f07d7f61de9a0248f450d024accb51b1906feac
Andres Gesteira <gesteira.pro@gmail.com>
2026-04-26 22:46:31 +0200

    refine

--- projects/awork-challenge/src/app/users/containers/users/users.component.ts
+++ projects/awork-challenge/src/app/users/containers/users/users.component.ts
@@ -1,4 +1,12 @@
-import { ChangeDetectionStrategy, Component, inject, signal, WritableSignal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  inject,
+  signal,
+  WritableSignal,
+} from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { User } from '../../models/user.model';
 import { UserService } from '../../services/user/user.service';
 
@@ -11,14 +19,14 @@ import { UserService } from '../../services/user/user.service';
   templateUrl: './users.component.html',
 })
 export class UsersComponent {
+  #destroyRef = inject(DestroyRef);
   #userService = inject(UserService);
   users: WritableSignal<User[]> = signal<User[]>([]);
 
   constructor() {
-    this.#userService.getUsers().subscribe((users: User[]) => this.users.set(users));
+    this.#userService
+      .getUsers()
+      .pipe(takeUntilDestroyed(this.#destroyRef))
+      .subscribe((users: User[]) => this.users.set(users));
   }
-  // // Remove this prop if this component contains no RxJS subscriptions.
-  // #destroyRef = inject(DestroyRef);
-  // // Remove this prop if this component contains no loading interface (e.g. spinner or progress).
-  // isLoading: WritableSignal<boolean> = signal<boolean>(true);
 }
