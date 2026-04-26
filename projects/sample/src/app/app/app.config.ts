12ee87c3baf2418cedb7538b96e9397d23c9a867
Andres Gesteira <gesteira.pro@gmail.com>
2026-04-26 21:11:55 +0200

    Update project name to "awork-challenge" and version to 3.0.0; add a blank line in app.config.ts for improved readability.

--- projects/sample/src/app/app/app.config.ts
+++ projects/sample/src/app/app/app.config.ts
@@ -6,6 +6,7 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient } from '@angular/common/http';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
