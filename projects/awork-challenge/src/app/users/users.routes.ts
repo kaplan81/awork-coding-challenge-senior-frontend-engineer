4ffc355134f5f941a338ab7970b6464aeedecbb2
Andres Gesteira <gesteira.pro@gmail.com>
2026-04-26 22:56:46 +0200

    format

--- projects/awork-challenge/src/app/users/users.routes.ts
+++ projects/awork-challenge/src/app/users/users.routes.ts
@@ -3,7 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () =>
-      import('./containers/users/users.component').then((m) => m.UsersComponent),
+    loadComponent: () => import('./containers/users/users.component').then((m) => m.UsersComponent),
   },
 ];
